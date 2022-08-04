b96cb37abd6531e4107aea22563779d42276067d
Gabriel Reis <gabrielr.nogueira2000@gmail.com>
2022-08-04 00:05:33 -0300

    evoluindo tela de buscar visualmente

--- lib/components/search_film_filters.dart
+++ lib/components/search_film_filters.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class SearchFilmFilters extends StatefulWidget {
-  const SearchFilmFilters({Key? key}) : super(key: key);
+  const SearchFilmFilters({
+    Key? key,
+    required this.searchFilm,
+  }) : super(key: key);
+
+  final void Function(Map<String, dynamic>) searchFilm;
 
   @override
   State<SearchFilmFilters> createState() => _SearchFilmFiltersState();
@@ -59,14 +64,15 @@ class _SearchFilmFiltersState extends State<SearchFilmFilters> {
     return false;
   }
 
-  void buscar_filme() {
+  void searchFilm() {
     var filtroFilme = {
       "nomefilme": nomeFilme != "" ? nomeFilme : null,
       "genero": genero != "Gênero" ? genero : null,
       "lancamento": lancamento != "Lançamento" ? lancamento : null,
-      "diretor": diretor != "Diretor" ? diretor : null
+      "diretor": diretor != "Diretor" ? diretor : null,
+      "quantidade": int.parse(nomeFilme)
     };
-    print(filtroFilme);
+    widget.searchFilm(filtroFilme);
   }
 
   Widget searchField() {
@@ -92,7 +98,7 @@ class _SearchFilmFiltersState extends State<SearchFilmFilters> {
         const SizedBox(width: 5),
         Expanded(
           child: ElevatedButton(
-            onPressed: filtroValido() ? buscar_filme : null,
+            onPressed: filtroValido() ? searchFilm : null,
             child: const Text(
               "Buscar",
               style: TextStyle(fontSize: 18),
@@ -103,7 +109,7 @@ class _SearchFilmFiltersState extends State<SearchFilmFilters> {
     );
   }
 
-  Widget filtroMenu(String holder, String categoria, List<String> opcoes) {
+  Widget menuFilter(String holder, String categoria, List<String> opcoes) {
     return Container(
       margin: const EdgeInsets.symmetric(
         vertical: 10,
@@ -155,10 +161,10 @@ class _SearchFilmFiltersState extends State<SearchFilmFilters> {
             searchField(),
             Row(
               children: [
-                Expanded(flex: 10, child: filtroMenu(genero, "G", generos)),
+                Expanded(flex: 10, child: menuFilter(genero, "G", generos)),
                 Expanded(
-                    flex: 9, child: filtroMenu(lancamento, "L", lancamentos)),
-                Expanded(flex: 11, child: filtroMenu(diretor, "D", diretores)),
+                    flex: 9, child: menuFilter(lancamento, "L", lancamentos)),
+                Expanded(flex: 11, child: menuFilter(diretor, "D", diretores)),
               ],
             )
           ],
