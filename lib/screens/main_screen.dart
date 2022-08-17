78ef3efee5cad93954e027cbcf0937832ddd2ea8
washington rocha <washingtonbdrocha@gmail.com>
2022-08-17 20:06:03 -0300

    commit

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -30,12 +30,6 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            /*IconButton(
-              onPressed: () {
-                _gotoConfigPage(context);
-              },
-              icon: const Icon(Icons.list_rounded),
-            ),*/
             Text(widget.title),
           ],
         ),
