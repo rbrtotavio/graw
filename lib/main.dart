8a106afb0a3ed34d4c6f915f09b7cbefa2c515ae
washington rocha <washingtonbdrocha@gmail.com>
2022-08-18 10:27:58 -0300

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:cinegraw_app/screens/search_film_screen.dart';
 import 'package:cinegraw_app/screens/config_screen.dart';
 import 'package:cinegraw_app/screens/login_register.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
