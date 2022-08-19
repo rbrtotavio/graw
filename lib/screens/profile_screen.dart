42d9181f2d86a285e72c347904d24546d3b71713
washington rocha <washingtonbdarocha@gmail.com>
2022-08-18 22:40:41 -0300

    correção conta github

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -20,7 +20,7 @@ class ProfilePage extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.search),
             onPressed: () {
-              _gotoReturn(context); //página inicial
+              _gotoReturn(context);
             },
           )
         ],
