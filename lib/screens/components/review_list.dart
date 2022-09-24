50e11d579aa93e86ea93b0036ef7425b677c7453
Roberto O. P. Leite <rbrtotavio@gmail.com>
2022-09-24 20:14:25 -0300

    Review List component refactor + Edit/Delete Button implemmentation

--- lib/screens/components/review_list.dart
+++ lib/screens/components/review_list.dart
@@ -1,14 +1,15 @@
+import 'package:cinegraw_app/applications/implementation/profile_app.dart';
 import 'package:cinegraw_app/utility/appthemes.dart';
 import 'package:cinegraw_app/models/review.dart';
 import 'package:flutter/material.dart';
 
 class ReviewList extends StatelessWidget {
-  const ReviewList(
-      {Key? key, required this.reviews, required this.isProfileScreen})
+  ReviewList({Key? key, required this.reviews, required this.isProfileScreen})
       : super(key: key);
 
   final List<Review> reviews;
   final bool isProfileScreen;
+  final ProfileApp _profileApp = ProfileApp();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +24,7 @@ class ReviewList extends StatelessWidget {
                 return Column(
                   children: [
                     Container(
-                      padding: EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
                         color: colorAppbar,
                         borderRadius: BorderRadius.circular(15),
@@ -32,18 +33,45 @@ class ReviewList extends StatelessWidget {
                       child: Column(
                         children: [
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                //"Ontem, às 22:00",
+                                reviews[index].reviewDate.toString(),
+                                style: TextStyle(color: colorBG),
+                              ),
+                              Flex(
+                                direction: Axis.horizontal,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                      padding: const EdgeInsets.all(8.0),
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.edit)),
+                                  IconButton(
+                                      padding: const EdgeInsets.all(8.0),
+                                      onPressed: () {
+                                        _profileApp.deleteReview(
+                                            reviews[index].reviewId);
+                                      },
+                                      icon: const Icon(Icons.delete))
+                                ],
+                              ),
+                            ],
+                          ),
+                          const Divider(
+                            color: colorBG,
+                          ),
+                          Row(
                             children: [
                               const Icon(
                                 Icons.person,
                                 size: 70,
                                 color: Colors.white,
                               ),
-                              const SizedBox(
-                                width: 10,
-                              ),
                               Container(
-                                margin: EdgeInsets.all(5),
-                                padding: EdgeInsets.all(5),
+                                margin: const EdgeInsets.all(8),
+                                padding: const EdgeInsets.all(8),
                                 child: Text(
                                   isProfileScreen
                                       ? reviews[index].filmName
@@ -54,40 +82,37 @@ class ReviewList extends StatelessWidget {
                                       color: Colors.white),
                                 ),
                               ),
-                              const SizedBox(
-                                width: 10,
-                              ),
                               Expanded(
-                                child: Container(
-                                  alignment: Alignment.centerRight,
-                                  margin: EdgeInsets.all(5),
-                                  padding: EdgeInsets.all(5),
-                                  child: Text(
-                                    reviews[index].rating.toString(),
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.w700,
-                                        fontSize: 20,
-                                        color: Colors.white),
-                                  ),
+                                child: Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        reviews[index].rating.toString(),
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 20,
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ],
                           ),
-                          Container(
-                            child: Text(
-                              reviews[index].review,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                              ),
+                          Text(
+                            reviews[index].review,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
                             ),
                           ),
                         ],
                       ),
                     ),
-                    const SizedBox(
-                      height: 5,
-                    ),
+                    const Divider(),
                   ],
                 );
               },
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinegraw_app/applications/implementation/profile_app.dart';
+import 'package:cinegraw_app/models/profile.dart';
 import 'package:cinegraw_app/screens/components/review_list.dart';
 import 'package:cinegraw_app/config/utilities.dart';
 import 'package:cinegraw_app/models/profile.dart';
