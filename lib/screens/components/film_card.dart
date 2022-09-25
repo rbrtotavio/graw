042e3bd651049a3aaf52200d214c8aa4324b43a9
Roberto O. P. Leite <rbrtotavio@gmail.com>
2022-09-25 01:33:11 -0300

    titleCard on long press in filmCard

--- lib/screens/components/film_card.dart
+++ lib/screens/components/film_card.dart
@@ -1,6 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cinegraw_app/applications/implementation/films_app.dart';
-import 'package:cinegraw_app/applications/implementation/profile_app.dart';
+import 'package:cinegraw_app/config/utilities.dart';
 import 'package:cinegraw_app/models/movieDB/film_movieDB.dart';
+import 'package:cinegraw_app/utility/appthemes.dart';
 import 'package:flutter/material.dart';
 
 class FilmCard extends StatefulWidget {
@@ -13,7 +15,7 @@ class FilmCard extends StatefulWidget {
 
 class _FilmCardState extends State<FilmCard> {
   final FilmsApp _filmsApp = FilmsApp();
-  final ProfileApp _profileApp = ProfileApp();
+  bool isTurned = false;
 
   void _gotoFilm() async {
     Navigator.pushNamed(
@@ -26,8 +28,58 @@ class _FilmCardState extends State<FilmCard> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onLongPress: () {
+        if (isTurned == true) {
+          isTurned = false;
+        } else {
+          isTurned = true;
+        }
+        setState(() {});
+      },
       onTap: _gotoFilm,
-      child: _filmsApp.getCardImage(widget.film.cardImagePath),
+      child: getFilmImg(),
+    );
+  }
+
+  dynamic getFilmImg() {
+    if (isTurned == false) {
+      return _filmsApp.getCardImage(widget.film.cardImagePath);
+    }
+    return getTitleCard();
+  }
+
+  Flex getTitleCard() {
+    return Flex(
+      direction: Axis.horizontal,
+      children: [
+        Container(
+          height: 200,
+          width: 133.3,
+          color: colorAppbar,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AutoSizeText(
+                    widget.film.title.toString(),
+                    style: Utilities.styleTitle,
+                    textAlign: TextAlign.center,
+                    maxLines: 2,
+                  ),
+                  Text(
+                    '(${widget.film.releaseDate!.year.toString()})',
+                    style: Utilities.styleRater,
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
